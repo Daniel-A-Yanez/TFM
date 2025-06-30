3375daed94624fbd924cae42b4568367c15f36f9
Daniel <danielalexander03@icloud.com>
2025-06-30 17:45:24 -0500

    Arreglos cosméticos

---
+++ src/app/components/acreditaciones/acreditaciones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acreditaciones',
+  imports: [],
+  templateUrl: './acreditaciones.component.html',
+  styleUrl: './acreditaciones.component.css'
+})
+export class AcreditacionesComponent {
+
+}
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -4,11 +4,12 @@ import { HeroSliderComponent } from '../../components/hero-slider/hero-slider.co
 import { FooterComponent } from '../../components/footer/footer.component';
 import { BloqueProgramaHomeComponent } from '../../components/bloque-programa-home/bloque-programa-home.component';
 import { NuestrosProgramasComponent } from '../../components/nuestros-programas/nuestros-programas.component';
+import { AcreditacionesComponent } from '../../components/acreditaciones/acreditaciones.component';
 
 
 @Component({
   selector: 'app-inicio',
-  imports: [MenuComponent, HeroSliderComponent, FooterComponent, BloqueProgramaHomeComponent, NuestrosProgramasComponent],
+  imports: [MenuComponent, HeroSliderComponent, FooterComponent, BloqueProgramaHomeComponent, NuestrosProgramasComponent, AcreditacionesComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
